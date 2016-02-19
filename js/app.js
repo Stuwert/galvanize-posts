e226331455f5a55ae1248245c3f427db51a86cdd
Stuart Urback <stuart@urback.net>
2016-02-19 12:52:49 -0700

    Add dynamic coloring

--- js/app.js
+++ js/app.js
@@ -8,8 +8,18 @@ app.controller('PostController', function($scope){
   $scope.post = {};
   $scope.posts = [];
   $scope.addPost= function(){
+    $scope.post.popularity = 0;
     $scope.posts.push($scope.post);
-    $scope.post = null;
+    $scope.post = {};
     console.log($scope.posts);
+  }
+  $scope.colorClass = function(popularity){
+    if (popularity > 0){
+      return 'green';
+    }else if (popularity < 0){
+      return 'red';
+    }else {
+      return 'black';
+    }
   }
 })

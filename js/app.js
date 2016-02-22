23293d7bcf352c3024a7e99cd0afa2f05e005637
Stuart Urback <stuart@urback.net>
2016-02-22 11:24:50 -0700

    Add sorting

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ app.controller('RedditController', function($scope){
   $scope.toggleForm = function(){
     $scope.formDisplay = !$scope.formDisplay;
   }
+  $scope.ordered = '-popularity'
   $scope.post = {};
   $scope.searchVal;
   $scope.posts = [{
@@ -16,7 +17,7 @@ app.controller('RedditController', function($scope){
       text: "I have no idea why you wouldn't believe this."
     }],
     title: 'I love dogs',
-    time: '12/27/2015',
+    time: new Date().setFullYear(2015, 6, 1),
     author: 'Bing Bong',
     description: "This is too cool to be real",
     image: 'http://media.mydogspace.com.s3.amazonaws.com/wp-content/uploads/2013/08/puppy-500x350.jpg'
@@ -29,7 +30,7 @@ app.controller('RedditController', function($scope){
       }
     }
     return false;
-  }
+  };
 
   $scope.comment = {};
 
@@ -43,6 +44,10 @@ app.controller('RedditController', function($scope){
     $scope.postForm.$setPristine();
     $scope.toggleForm();
   }
+  $scope.updateOrder = function(newOrder){
+    $scope.ordered = newOrder;
+  }
+
   $scope.colorClass = function(popularity){
     if (popularity > 0){
       return 'green';

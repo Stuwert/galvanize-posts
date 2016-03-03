33a4d60457b5b9859d116ba94fac7d2ca44caafb
Stuart Urback <stuart@urback.net>
2016-03-02 21:09:28 -0700

    Final commit for the day

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('galvanizeeats', []);
+var app = angular.module('galvanize-posts', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/allposts.html'
+    })
+    .when('/posts/new', {
+      templateUrl: '/views/createpost.html'
+    })
+})
